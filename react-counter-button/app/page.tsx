091c3acffdfe857f1ad68e7ab00ca5b87b26e79e
Ricardo Gonzalez <ricardojoel.gonzalezv@gmail.com>
2024-07-13 13:29:23 -0600

    Feat: Create Independent State Button component

--- react-counter-button/app/page.tsx
+++ react-counter-button/app/page.tsx
@@ -1,21 +1,26 @@
 "use client";
 
-import Button from "@/components/button";
+import { ButtonSharedState } from "@/components/button-shared-state";
+import { ButtonIndependentState } from "@/components/button-independent-state";
 import Image from "next/image";
 import { useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(0);
 
-    function handleClick() {
-        setCount(count + 1);
-    }
+  function handleClick() {
+    setCount(count + 1);
+  }
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-3xl font-bold">Hello World</h1>
-      <Button count={count} onClick={handleClick}/>
-      <Button count ={count} onClick={handleClick}/>
+    <main className="flex min-h-screen flex-col items-center p-24 gap-4">
+      <h1 className="text-3xl font-bold">React Counter Buttons</h1>
+      <h2 className="text-xl">Buttons with shared state</h2>
+      <ButtonSharedState count={count} onClick={handleClick} />
+      <ButtonSharedState count={count} onClick={handleClick} />
+      <h2 className="text-xl">Buttons with independent state</h2>
+      <ButtonIndependentState />
+      <ButtonIndependentState />
     </main>
   );
 }
